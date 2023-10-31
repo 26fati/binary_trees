18d8d1458718cff033b087ed453daabdd71d8fae
26fati <fatibeautiful1@gmail.com>
2023-10-31 22:38:03 +0100

    task 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_sibling - finds 'sibling' to a node, sharing the same parent
+ * @node: node to check
+ * Return: pointer to node sharing same parent as `node`, or NULL if none found
+ * or `node` is NULL
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *parent;
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	parent = node->parent;
+	if (node == parent->left)
+		return (parent->right);
+	else if (node == parent->right)
+		return (parent->left);
+	return (NULL);
+}
