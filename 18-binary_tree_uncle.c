9880fae06152170bfec520faf96b3a46215f8fe8
26fati <fatibeautiful1@gmail.com>
2023-10-31 22:51:22 +0100

    task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -29,6 +29,8 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	if (node == NULL)
+		return (NULL);
 	if (node->parent)
 		return (binary_tree_sibling(node->parent));
 	return (NULL);
